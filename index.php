6ed8c2cb5f2dce3e0ce63499eede16fd9eece573
GeoffroyDutot <dutot.geoffroyplaposte.net>
2018-12-04 15:13:54 +0100

    Association entre person et dog

--- index.php
+++ index.php
@@ -12,15 +12,23 @@ class Person {
     }
     public function buy(Dog $pDog) {
         $this->dog = $pDog;
+        $this->dog->setOwner($this);
     }
 }
 class Dog {
+    private $owner;
     private $name;
     public function __construct(string $pName) {
         $this->name = $pName;
     }
     public function getName(): string {
         return $this->name;
+    }
+    public function setOwner(Person $pOwner) {
+        $this->owner = $pOwner;
+    }
+    public function speak(): string {
+        return "Mon maitre est JD et je suis J";
     }
 }
 $person = new Person("John", "Doe");

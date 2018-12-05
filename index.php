a1a4c2413b60f0b23183c2a3938fe292f25b28bd
GeoffroyDutot <dutot.geoffroyplaposte.net>
2018-12-05 16:39:20 +0100

    Ajout du try catch

--- index.php
+++ index.php
@@ -1,32 +1,51 @@
 <?php
-interface IAction {
-    public function avancer();
+class NoDogWithPersonException extends Exception { }
+interface IActions {
+    public function seMarier($p);
 }
-abstract class Vehicule implements IAction {
-//    public function avancer() {
-//        var_dump("J'avance...");
-//    }
-}
-class Car extends Vehicule {
-    public function avancer() {
-        var_dump("Je suis un Car et j'avance");
+class Person implements IActions {
+    private $partner;
+    private $firstname;
+    private $lastname;
+    public function __construct($pFirstname, $pLastname) {
+        $this->firstname = $pFirstname;
+        $this->lastname = $pLastname;
+    }
+    public function setLastname($p) {
+        $this->lastname = $p;
+    }
+    public function seMarier($p) {
+        if(!$p instanceof Person) {
+            throw new NoDogWithPersonException("On ne peut pas marier une Person avec un Dog.");
+        }
+        $p->setLastname($this->lastname);
+        $this->partner = $p;
     }
 }
-class Bike extends Vehicule {
-    public function avancer() {
-        var_dump("Je suis un Bike et j'avance");
+class Dog implements IActions {
+    private $partner;
+    private $name;
+    public function __construct($pName) {
+        $this->name = $pName;
+    }
+    public function seMarier($p) {
     }
 }
-class User implements IAction {
-    private $vehicule;
-    public function setVehicule(IAction $pVehicule) {
-        $this->vehicule = $pVehicule;
-    }
-    public function avancer() {
-        $this->vehicule->avancer();
-    }
+$boby = new Dog("Boby");
+$john = new Person("John", "Doe");
+$jane = new Person("Jane", "Die");
+try {
+    $john->seMarier($boby);
+} catch (NoDogWithPersonException $e) {
+    var_dump($e->getMessage());
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+} finally {
+    var_dump("Mon pote à fait une erreur !!!!");
 }
-$car = new Car();
-$user = new User();
-$user->setVehicule($car);
-$user->avancer();
+try {
+    $john->seMarier($jane);
+} catch (NoDogWithPersonException $e) {
+    var_dump($e->getMessage());
+}
+var_dump($john);

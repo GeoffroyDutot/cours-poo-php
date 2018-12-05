43bac64bcfa2a9a023d6f197a92b0383fe665a75
GeoffroyDutot <dutot.geoffroyplaposte.net>
2018-12-05 13:08:05 +0100

    Ajout extension avec le mot clé final

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
 <?php
-class Mammal {
+class Animal {
     private $years = 0;
     public function __construct(int $pYears) {
         $this->years = $pYears;
     }
-    /**
-     * @return int
-     */
-    public function getYears(): int
-    {
-        return $this->years;
-    }
-    /**
-     * @param int $years
-     */
-    public function setYears(int $years): void
-    {
-        $this->years = $years;
-    }
 }
-class Person extends Mammal {
+class Mammal extends Animal { }
+class Oviparous extends Animal {
+    private $isMoult = false;
+}
+final class Person extends Mammal {
     private $firstname;
     private $lastname;
     public function __construct(string $pFirstname, string $pLastname, int $pYears) {
@@ -28,7 +18,14 @@ class Person extends Mammal {
         $this->lastname = $pLastname;
     }
 }
-class Dog extends Mammal {
+final class Dog extends Mammal {
+    private $name;
+    public function __construct(string $pName, int $pYears) {
+        parent::__construct($pYears);
+        $this->name = $pName;
+    }
+}
+final class Gecko extends Oviparous {
     private $name;
     public function __construct(string $pName, int $pYears) {
         parent::__construct($pYears);
@@ -37,5 +34,7 @@ class Dog extends Mammal {
 }
 $person = new Person("John", "Doe", 1);
 var_dump($person);
-$dog = new Dog("Boby", 1);
-var_dump($dog);
+//$dog = new Dog("Boby", 1);
+//var_dump($dog);
+$gecko = new Gecko("Geek", 1);
+var_dump($gecko);
